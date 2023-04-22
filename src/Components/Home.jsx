9f1ee84f9767db151a7cd45d10614db4081aa495
kajal1801 <anshikajais2001@gmail.com>
2023-04-22 21:34:34 +0530

    Added Home and Tasks

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,63 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Task from './Task.jsx'
 
 const Home = () => {
-  // const [inputValue, setInputValue] = useState(0)
+  const [tasks, setTasks] = useState(localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [])
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if(title !== '' && description !== ''){
+      setTasks([...tasks, { title, description }])
+      setTitle('')
+      setDescription('')
+    }
+    else{
+      alert('Please fill the fields')
+    }
+  }
+
+  const deleteTask = (index) => {
+    const filterTasks = tasks.filter((item, i) => i !== index)
+    setTasks(filterTasks)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
   return (
     <div className="container">
       <h1>Add your Goals here</h1>
-      <form>
-        <input type="text" placeholder='Title' />
-        <textarea placeholder='Description'></textarea>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder='Title'
+          value={title}
+          onChange={(e) =>
+            setTitle(e.target.value)
+          } />
+        <textarea placeholder='Description'
 
-        <button type='submit'>Submit</button>
+          value={description}
+          onChange={(e) =>
+            setDescription(e.target.value)
+          }
+        ></textarea>
+
+        <button className='btn' type='submit'>Submit</button>
       </form>
 
-      <Task />
+      {tasks.map((item, index) => (
+        <Task
+          key={index}
+          title={item.title}
+          description={item.description}
+          deleteTask={deleteTask}
+          index={index}
+        />
+      ))}
     </div>
   )
 }
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
-const Task = () => {
-  return (
-    <div className='task'>
-        <div>
-            <p>Title</p>
-            <span>Description</span>
-            <button>-</button>
+const Task = ({title, description, deleteTask, index}) => {
+    const trash = <FontAwesomeIcon icon={faTrashCan} />
+
+    return (
+        <div className='task'>
+            <div>
+                <div >
+                    <p>{title}</p>
+                    <span>{description}</span>
+                </div>
+                <button id='trash' onClick={() => deleteTask(index)}>
+                    {trash}
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Task
