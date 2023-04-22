6322ccc1d4597894d92487f6179d85d7cd5d4e4f
kajal1801 <anshikajais2001@gmail.com>
2023-04-22 22:00:25 +0530

    Removed Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import Header from './Components/Header.js'
-import Footer from './Components/Footer.js'
 import Home from './Components/Home.jsx'
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div>
       <Header />
       <Home />
-      <Footer />
     </div>
   );
 }
